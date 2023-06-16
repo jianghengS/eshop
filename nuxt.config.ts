08a6dae9188229b1936c92919e2eced26252e7ec
jianghengS <1456949551@qq.com>
2023-06-16 15:23:37 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  head: {
+    script: [
+      {
+        src: "//515012316.collect.igodigital.com/collect.js",
+      },
+    ],
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
 });
